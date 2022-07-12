a3b6c0959f6b3c8e7167a2c725c3933f6b7247c9
laril <lari.lantonen@tuni.fi>
2022-07-12 19:00:02 +0300

    2.4: kurssitiedot step9 ja 2.5

--- osa2/kurssitiedot/src/components.js/Course.js
+++ osa2/kurssitiedot/src/components.js/Course.js
@@ -3,16 +3,7 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Part = ({part}) => {
-    return(
-        <p>
-            {part.name} {part.exercises}
-        </p>
-    )
-    
-}
-
-const Course = (course) => {
+const Course = ({course}) => {
     return (
             <div>
                 <Header name={course.name} />
