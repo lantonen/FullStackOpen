261ad6400b44cad5e28154d5c186ec03f962868a
laril <lari.lantonen@tuni.fi>
2022-07-31 10:32:42 +0300

    2.9: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -10,10 +10,14 @@ const Person = ({person}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [condition, setCondition] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -32,6 +36,15 @@ const App = () => {
     } 
   }
 
+  const personsToShow = (condition === '')
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(condition.toLowerCase()) === true)
+
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setCondition(event.target.value)
+  }
+
   const handlePersonChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -45,18 +58,20 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input onChange={handleSearch}></input></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handlePersonChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div><button type="submit">add</button>
-        </div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => 
+      <div>
+        {personsToShow.map(person => 
           <Person key={person.name} person={person} />
         )}
-      </ul>
+      </div>
+      
 
     </div>
   )
