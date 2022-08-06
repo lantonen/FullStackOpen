4bc249c7ec14cb1599b48c9d31a9fde31bb532ba
laril <lari.lantonen@tuni.fi>
2022-08-06 12:32:53 +0300

    2.12* maiden tiedot, step1

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,28 +1,53 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const View = ({listOfCountries}) => {
-  console.log(listOfCountries)
-  return (
-    <div>
-      {listOfCountries.map(country => 
-      <Country key={country.name.official} object={country} />
-      )}
-    </div>
-  )
+const View = ({ listOfCountries }) => {
+  console.log(listOfCountries.length)
+  if (listOfCountries.length === 1) {
+    return (
+      <div>
+        <Country key={listOfCountries.at(0).name.official} object={listOfCountries.at(0)} />
+      </div>
+    )
+  }
+  else if (2 <= listOfCountries.length && listOfCountries.length <= 10){
+    return (
+      <div>
+        {listOfCountries.map(country =>
+          <div key={country.name.official}>{country.name.common}</div>
+        )}
+      </div>
+    )
+  }
+  else if(listOfCountries.length > 10){
+    return(
+      <div>Too many matches, specify another filter</div>
+    )
+  }
 }
 
-const Country = ({object}) => {
+const Country = ({ object }) => {
+  console.log(object)
   return (
-    <div>{object.name.official}</div>
+    <div>
+      <h1>{object.name.common}</h1>
+      <div>capital: {object.capital}</div>
+      <div>area: {object.area}</div>
+      <h2>languages:</h2>
+      {Object.values(object.languages).map(language =>
+        <div>{language}</div>
+      )}
+      <img src={object.flags.png} alt={"flag"}></img>
+    </div>
+    
   )
 }
 
 const App = () => {
-  
-  const [condition, setCondition] = useState('') 
-  const [countries, setCountries] = useState([]) 
+
+  const [condition, setCondition] = useState('')
+  const [countries, setCountries] = useState([])
 
   const handleInputChange = (event) => {
     console.log(event.target.value)
@@ -42,7 +67,7 @@ const App = () => {
 
   const countriesToShow = (condition === '')
     ? countries
-    : countries.filter(country => country.name.official.toLowerCase().includes(condition.toLowerCase()) === true)
+    : countries.filter(country => country.name.common.toLowerCase().includes(condition.toLowerCase()) === true)
 
   return (
     <div>
@@ -52,5 +77,5 @@ const App = () => {
       <View listOfCountries={countriesToShow} ></View>
     </div>
   )
-  }
+}
 export default App;
